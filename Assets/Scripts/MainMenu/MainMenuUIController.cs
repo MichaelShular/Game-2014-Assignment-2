919ad66c47720501de2ccccfd343bf710b375130
Mick <mickx.shular@gmail.com>
2021-12-13 20:10:45 -0500

    changed Instruction UI

--- Assets/Scripts/MainMenu/MainMenuUIController.cs
+++ Assets/Scripts/MainMenu/MainMenuUIController.cs
@@ -7,10 +7,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
+
 public class MainMenuUIController : MonoBehaviour
 {
-    [SerializeField] private Canvas instructionsUI; 
-   
+    [SerializeField] private List<TextMeshProUGUI> instructionScripts;
+    [SerializeField] private Canvas instructionsUI;
+    private int currentInstuction;
+
+    private void Start()
+    {
+        currentInstuction = 0;
+        instructionScripts[currentInstuction].enabled = true;
+    }
     public void changeToGameMenu()
     {
         SceneManager.LoadScene("GameMenu");
@@ -19,9 +28,31 @@ public class MainMenuUIController : MonoBehaviour
     public void openInstructions()
     {
         instructionsUI.enabled = !instructionsUI.enabled;
+        currentInstuction = 0;
     }
     public void quitGame()
     {
         Application.Quit();
+    }
+
+    public void changeInstuctionText()
+    {
+        currentInstuction++;
+        if(currentInstuction == instructionScripts.Count)
+        {
+            currentInstuction = 0;
+        }
+
+        if(currentInstuction == 0)
+        {
+            instructionScripts[currentInstuction].enabled = true;
+            instructionScripts[instructionScripts.Count -1].enabled = false;
+        }
+        else
+        {
+            instructionScripts[currentInstuction].enabled = true;
+            instructionScripts[currentInstuction - 1].enabled = false;
+        }
+        
     }
 }
